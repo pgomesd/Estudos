49e50a9c5cf8a1b574665232a8d14fdea6bbfacc
pvbIx <pablogomes.cel@gmail.com>
2022-10-10 07:19:41 -0300

    operadores de atribuição

--- Conhecendo a Plataforma .NET com C#/Sintaxe e Tipos de Dados/Program.cs
+++ Conhecendo a Plataforma .NET com C#/Sintaxe e Tipos de Dados/Program.cs
@@ -2,57 +2,88 @@
 
 
 
-int a = 10;
-int b = 20;
-
-int c = a + b;
-
-c = c + 5;
-
-Console.WriteLine(c);
 
 
 
 
-
-// DateTime dataAtual = DateTime.Now.AddDays(5);
-// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
-
-
+//Ordem dos operadores
+    //  double a = 4 / (2 + 2);
+    //  Console.WriteLine(a);
 
 
+// Cast - Casting
+     // int a = Convert.ToInt32(null);
+     // int a = int.Parse(null);
+        // Console.WriteLine(a);
+// Cast para string 
+    // int inteiro = 5;
+    // string a = inteiro.ToString();
+        // Console.WriteLine(a);
+// Cast implícito
+    // int a = 5;
+    // double b = a;
 
-// string apresentacao = "Olá, seja bem vindo";
+    // long a = long.MaxValue;
+    // int b = Convert.ToInt32(a);
 
-// int quantidade = 1;
-// Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+    // int a = int.MaxValue;
+    // long b = a;
+        // Console.WriteLine(b);
+// Cast de maneira segura
+    // string a = "15-";
+    // int b = 0;
+    // int.TryParse(a, out int b);
 
-// quantidade = 10;
-// Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+        // Console.WriteLine(b);
+        // Console.WriteLine("Conversão realizada com sucesso!");
+//
 
-// double altura = 1.80;
+//  Combinando Operadores
+        // int a = 10;
+        // int b = 20;
 
-// decimal preco = 1.80M;
+        // int c = a + b;
 
-// bool condicao = true;
+        // c = c + 5; //30 + 5
+        //c += 5;
+        //c -= 5;
+        //c *= 5; 
 
-
-
-
-
-
-// Console.WriteLine(apresentacao);
-// Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
-// Console.WriteLine("Valor da váriavel altura: " + altura.ToString("0.00"));
-// Console.WriteLine("Valor da váriavel preco: " + preco);
-// Console.WriteLine("Valor da váriavel condicao: " + condicao);
+            // Console.WriteLine(c);
+//
 
 
 
 
+// Introdução ao .NET com C#
+    // DateTime dataAtual = DateTime.Now.AddDays(5);
+    // Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
-// Pessoa pessoa1 = new Pessoa();
 
-// pessoa1.Nome = "Pablo";
-// pessoa1.Idade = 24;
-// pessoa1.Apresentar();
+    // string apresentacao = "Olá, seja bem vindo";
+
+    // int quantidade = 1;
+    // Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+
+    // quantidade = 10;
+    // Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+
+    // double altura = 1.80;
+
+    // decimal preco = 1.80M;
+
+    // bool condicao = true;
+
+
+    // Console.WriteLine(apresentacao);
+    // Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+    // Console.WriteLine("Valor da váriavel altura: " + altura.ToString("0.00"));
+    // Console.WriteLine("Valor da váriavel preco: " + preco);
+    // Console.WriteLine("Valor da váriavel condicao: " + condicao);
+
+    // Pessoa pessoa1 = new Pessoa();
+
+    // pessoa1.Nome = "Pablo";
+    // pessoa1.Idade = 24;
+    // pessoa1.Apresentar();
+//
