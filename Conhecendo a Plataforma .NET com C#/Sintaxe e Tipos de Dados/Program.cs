42b2e5c8861a086c6005204b96fd50ba72e4b376
pvbIx <pablogomes.cel@gmail.com>
2022-10-10 05:42:55 -0300

    conhecendo dotnet e csharp

---
+++ Conhecendo a Plataforma .NET com C#/Sintaxe e Tipos de Dados/Program.cs
@@ -0,0 +1,54 @@
+﻿using ExemploFundamentos.Models;
+
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+
+
+
+
+
+
+
+
+
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+// Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+
+// quantidade = 10;
+// Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+
+
+
+
+
+
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+// Console.WriteLine("Valor da váriavel altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da váriavel preco: " + preco);
+// Console.WriteLine("Valor da váriavel condicao: " + condicao);
+
+
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Pablo";
+// pessoa1.Idade = 24;
+// pessoa1.Apresentar();
