dfa5fbee829a96b2273ed6e97de5d106e35808bd
pvbIx <pablogomes.cel@gmail.com>
2022-10-10 07:24:41 -0300

    rename

---
+++ dotNet-C#/Program.cs
@@ -0,0 +1,89 @@
+﻿using ExemploFundamentos.Models;
+
+
+
+
+
+
+
+//Ordem dos operadores
+    //  double a = 4 / (2 + 2);
+    //  Console.WriteLine(a);
+
+
+// Cast - Casting
+     // int a = Convert.ToInt32(null);
+     // int a = int.Parse(null);
+        // Console.WriteLine(a);
+// Cast para string 
+    // int inteiro = 5;
+    // string a = inteiro.ToString();
+        // Console.WriteLine(a);
+// Cast implícito
+    // int a = 5;
+    // double b = a;
+
+    // long a = long.MaxValue;
+    // int b = Convert.ToInt32(a);
+
+    // int a = int.MaxValue;
+    // long b = a;
+        // Console.WriteLine(b);
+// Cast de maneira segura
+    // string a = "15-";
+    // int b = 0;
+    // int.TryParse(a, out int b);
+
+        // Console.WriteLine(b);
+        // Console.WriteLine("Conversão realizada com sucesso!");
+//
+
+//  Combinando Operadores
+        // int a = 10;
+        // int b = 20;
+
+        // int c = a + b;
+
+        // c = c + 5; //30 + 5
+        //c += 5;
+        //c -= 5;
+        //c *= 5; 
+
+            // Console.WriteLine(c);
+//
+
+
+
+
+// Introdução ao .NET com C#
+    // DateTime dataAtual = DateTime.Now.AddDays(5);
+    // Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+    // string apresentacao = "Olá, seja bem vindo";
+
+    // int quantidade = 1;
+    // Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+
+    // quantidade = 10;
+    // Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+
+    // double altura = 1.80;
+
+    // decimal preco = 1.80M;
+
+    // bool condicao = true;
+
+
+    // Console.WriteLine(apresentacao);
+    // Console.WriteLine("Valor da váriavel qunatidade: " + quantidade);
+    // Console.WriteLine("Valor da váriavel altura: " + altura.ToString("0.00"));
+    // Console.WriteLine("Valor da váriavel preco: " + preco);
+    // Console.WriteLine("Valor da váriavel condicao: " + condicao);
+
+    // Pessoa pessoa1 = new Pessoa();
+
+    // pessoa1.Nome = "Pablo";
+    // pessoa1.Idade = 24;
+    // pessoa1.Apresentar();
+//
