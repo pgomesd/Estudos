d22140608324bf8d9a2e59d910aa33482c2953ec
pvbIx <pablogomes.cel@gmail.com>
2022-10-12 05:09:52 -0300

    estruturas de repetição

--- dotNet-C#/Models/Pessoa.cs
+++ dotNet-C#/Models/Pessoa.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ExemploFundamentos.Models
+namespace dotNetC.Models
 {
     public class Pessoa
     {
