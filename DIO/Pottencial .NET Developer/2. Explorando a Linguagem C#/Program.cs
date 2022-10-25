fdab5df702e01824a0a42cdb78b09a1127fb315c
pgomesd <pablogomes.cel@gmail.com>
2022-10-25 11:05:40 -0300

    add Exceções e Coleções

--- DIO/Pottencial .NET Developer/2. Explorando a Linguagem C#/Program.cs
+++ DIO/Pottencial .NET Developer/2. Explorando a Linguagem C#/Program.cs
@@ -1,43 +1,41 @@
 ﻿using Explorando.Models;
 using System.Globalization;
-using System;
-
-string dataString = "2022-04-170 18:00";
-
-bool sucesso = DateTime.TryParseExact(dataString,
-                      "yyyy-MM-dd HH:mm",
-                      CultureInfo.InvariantCulture,
-                      DateTimeStyles.None, out DateTime data);
 
 
-if (sucesso)
-{
-    Console.WriteLine($"Conversão com sucesso! Data: {data}");
-}
-else
-{
-    Console.WriteLine($"{dataString} não é uma data válida.");
-}
+Dictionary<string, string> estados = new Dictionary<string, string>();
 
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
 
+Console.WriteLine(estados["MG"]);
 
+// foreach(var item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+// }
 
+// Console.WriteLine("------------");
 
+// estados.Remove("BA");
+// estados["SP"] = "São Paulo - valor alterado";
 
+// foreach(var item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+// }
 
+// string chave = "BA";
+// Console.WriteLine("Verificando o elemento:");
 
-
-
-
-
-
-
-
-
-
-
-
-
+// if (estados.ContainsKey(chave))
+// {
+//     Console.WriteLine ($"Valor existente: {chave}");
+// }
+// else
+// {
+//     Console.WriteLine ($"Valor inexistente: {chave}");
+// }
 
 
 
@@ -53,6 +51,182 @@ else
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// PILHA (STACK)
+// Stack<int> pilha = new Stack<int>();
+
+// pilha.Push(4);
+// pilha.Push(6);
+// pilha.Push(8);
+// pilha.Push(10);
+
+// foreach(int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+
+// pilha.Push(20);
+
+// foreach(int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FILA (QUEUE)
+// Queue<int> fila = new Queue<int>();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+// foreach(int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Revomendo o elemento: {fila.Dequeue()}");
+// fila.Enqueue(10);
+
+// foreach(int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EXCEÇÕES E COLEÇÕES
+//EXCEÇÕES
+// try
+// {
+
+//     string[] linhas = File.ReadAllLines("Arquivos/pasta/arquivo_Leitura.txt");
+
+//     foreach (string linha in linhas)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// }
+// catch(FileNotFoundException ex)
+// {
+//     Console.WriteLine($"Arquivo não encontrado. {ex.Message}");
+// }
+// catch(DirectoryNotFoundException ex)
+// {
+//     Console.WriteLine($"Diretório não encontrado. {ex.Message}");
+// }
+// catch(Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+// }
+// finally
+// {
+//     Console.WriteLine("Chegou até aqui");
+// }
+//
+// THROW
+//new ExemploExcecao().Metodo1();
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MANIPULANDO VALORES
+//DATETIME
+// string dataString = "2022-04-170 18:00";
+
+// bool sucesso = DateTime.TryParseExact(dataString,
+//                       "yyyy-MM-dd HH:mm",
+//                       CultureInfo.InvariantCulture,
+//                       DateTimeStyles.None, out DateTime data);
+
+
+// if (sucesso)
+// {
+//     Console.WriteLine($"Conversão com sucesso! Data: {data}");
+// }
+// else
+// {
+//     Console.WriteLine($"{dataString} não é uma data válida.");
+// }
+//
+// FORMATAÇÃO E REPRESENTAÇÃO
 // CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
 // decimal valorMonetario = 1582.40M;
@@ -65,88 +239,20 @@ else
 
 // int numero = 123456;
 // Console.WriteLine(numero.ToString("##-##-##"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//
+// CONCATENANDO
 // int numero1 = 10;
 // string numero2 = "20";
 
 // string resultado = numero1 + numero2;
 
 // Console.WriteLine(resultado);
+//
+//
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// PROPRIEDADES, METODOS E CONSTRUTORES
 // Pessoa p1 = new Pessoa(nome: "Pablo", sobrenome: "Gomes");
 // Pessoa p2 = new Pessoa(nome: "Eduardo", sobrenome: "Neves Queiroz");
 
@@ -158,7 +264,7 @@ else
 // cursoDeIngles.AdicionarAluno(p1);
 // cursoDeIngles.AdicionarAluno(p2);
 // cursoDeIngles.ListarAlunos();
-
+//
 
 
 
