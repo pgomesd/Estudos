75b749ba1e667a7765fc264a5468ca5bf7533a32
pgomesd <pablogomes.cel@gmail.com>
2022-10-25 21:56:36 -0300

    add Nuget, serializar e atributos

--- DIO/Pottencial .NET Developer/2. Explorando a Linguagem C#/Program.cs
+++ DIO/Pottencial .NET Developer/2. Explorando a Linguagem C#/Program.cs
@@ -1,5 +1,4 @@
-﻿using Explorando.Models;
-using System.Globalization;
+﻿
 
 
 
@@ -30,15 +29,37 @@ using System.Globalization;
 
 
 
+// DESERIALIZAÇÃO
+// using Explorando.Models;
+// using System.Globalization;
+// using Newtonsoft.Json;
+// string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
 
+// List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
 
+// foreach (Venda venda in listaVenda)
+// {
+//     Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda}");
+// }
 
+// SERIALIZAÇÃO
+// DateTime dataAtual = DateTime.Now;
 
+// List<Venda> listaVendas = new List<Venda>();
 
+// Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+// Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
 
+// listaVendas.Add(v1);
+// listaVendas.Add(v2);
 
+// string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 
+// File.WriteAllText("Arquivos/vendas.json", serializado);
 
+// Console.WriteLine(serializado);
+
+//
 // IF Ternário
 // int numero = 10;
 // bool ehPar = false;
